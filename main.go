059928806c19cb18f1dfe64ed7bcda33fff08b67
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-08-23 17:16:55 +0300

    feat : added some basic HTTP request code

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	holders := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://golang.com",
+		"http://stackoverflow.com",
+		"http://amazon.com",
+	}
+
+	for _, link := range holders {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println("UhOh, website is down: ", link)
+	} else {
+		fmt.Println("No issues!")
+	}
+}
